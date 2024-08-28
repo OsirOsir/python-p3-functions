3b8ea7ec26cae38dbbaec2631b396af39cc30713
Philip osir Otieno <philiposir2@gmail.com>
2024-08-28 05:20:03 +0300

    updates

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python3
 
+
 def greet_programmer():
-    pass
+    print("Hello, programmer!")  # Add the exclamation mark
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")  # Add the exclamation mark
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")  # Add the exclamation mark
 
 def add(num1, num2):
-    pass
+    return num1 + num2  # This function is correct as it is
 
 def halve(number):
-    pass
+    if type(number) not in (int, float): 
+        return None  # Return None if the input is not a number
+    return number / 2  # Return half of the number
